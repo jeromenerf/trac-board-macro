13b083fc51dc2c887807fbd96e5bcf10dec95404
Jerome Andrieux | af83 <jerome.andrieux@af83.com>
2013-02-06 11:18:39 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ trac-board-macro
 ================
 
 A simple trac 0.12 Board() macro to insert a basic 3|4 column tickets view for a given milestone.
+
+Once installed in your trac env plugins/ directory, you just need to insert
+`[[Board(name of the milestone)]]` in a wiki page and voilà!
+
+http://mackeblog.blogspot.fr/2011/09/trac-and-kanban-redux.html
+provided the idea and a template.
---
+++ boardmacro.py
@@ -0,0 +1,65 @@
+from genshi.core import Markup
+from trac.wiki.macros import WikiMacroBase
+from trac.wiki import Formatter
+import StringIO
+
+class BoardMacro(WikiMacroBase):
+    """
+    Returns a 4 columns board for the provided milestone
+    """
+    def expand_macro(self, formatter, name, text, args):
+        """
+        Returns some wiki text, interpreted
+        """
+        wikitext="""{{{
+#!div style='float:left; width:23%%'
+
+= TODO =
+
+[[TicketQuery(status=new|reopened,milestone=%(milestone)s,format=count)]]
+
+[[TicketQuery(status=new|reopened,order=priority,milestone=%(milestone)s,format=table,col=summary,priority)]]
+
+}}}
+
+
+{{{
+#!div style='float:left; width:23%%' 
+
+= DOING =
+[[TicketQuery(status=accepted|assigned,milestone=%(milestone)s,keywords!~=test,format=count)]]
+
+[[TicketQuery(status=accepted|assigned,order=priority,milestone=%(milestone)s,keywords!~=test,format=table,col=summary|owner)]]
+
+}}}
+
+
+{{{
+#!div style='float:left; width:23%%' 
+
+= TEST =
+[[TicketQuery(status=accepted|assigned,keywords~=test,milestone=%(milestone)s,format=count)]]
+
+[[TicketQuery(status=accepted|assigned,order=priority,keywords~=test,milestone=%(milestone)s,format=table,col=summary|owner)]]
+
+}}}
+
+
+{{{
+#!div style='float:left; width:23%%' 
+
+= DONE =
+[[TicketQuery(status=closed,milestone=%(milestone)s,format=count)]]
+
+[[TicketQuery(status=closed,milestone=%(milestone)s,format=table,col=resolution|summary)]]
+
+}}}
+
+{{{
+#!div style='clear:both'
+}}}""" % {'milestone': text}
+
+        out = StringIO.StringIO()
+        Formatter(self.env, formatter.context).format(wikitext, out)
+        Markup(out.getvalue()) 
+        return Markup(out.getvalue())
